ef105a8ca771c374da83db975dc115d8ec38917a
Wasim <wasimwik919@gmail.com>
2021-03-01 00:19:13 +0530

    fetching messages logic added

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,12 +1,29 @@
 import React, { useLayoutEffect, useState } from "react"
-import { StyleSheet, Text, TextInput, View, SafeAreaView, KeyboardAvoidingView, Platform, ScrollView, TouchableOpacity } from "react-native"
+import {
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+	SafeAreaView,
+	KeyboardAvoidingView,
+	Platform,
+	ScrollView,
+	TouchableOpacity,
+	Keyboard,
+	TouchableWithoutFeedback
+} from "react-native"
 import { Avatar } from "react-native-elements"
 import { StatusBar } from "expo-status-bar"
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons"
+import firebase from "firebase"
+import { db, auth } from "../firebase"
 
 const ChatScreen = ({ navigation, route }) => {
-	const { id, chatName } = route.params
 	const [input, setInput] = useState("")
+	const [messages, setMessages] = useState([])
+
+	const { id, chatName } = route.params
+
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			title: chatName,
@@ -40,27 +57,58 @@ const ChatScreen = ({ navigation, route }) => {
 			)
 		})
 	}, [navigation])
-	const sendMessage = () => {}
+	const sendMessage = () => {
+		Keyboard.dismiss()
+		db.collection("chats").doc(id).collection("messages").add({
+			timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+			message: input,
+			displayName: auth.currentUser.displayName,
+			email: auth.currentUser.email,
+			photoURL: auth.currentUser.photoURL
+		})
+		setInput("")
+	}
+	useLayoutEffect(() => {
+		const unsubscribe = db
+			.collection("chats")
+			.doc(id)
+			.collection("messages")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) =>
+				setMessages(
+					snapshot.docs.map((doc) => ({
+						id: doc.id,
+						data: doc.data()
+					}))
+				)
+			)
+		return unsubscribe
+	}, [route])
+	console.log(messages)
 	return (
 		<SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
 			<StatusBar style="light" />
 			<KeyboardAvoidingView
 				style={styles.container}
 				behavior={Platform.OS === "android" ? "height" : "padding"}
-				keyboardVerticalOffset={90}
+				keyboardVerticalOffset={100}
 			>
-				<ScrollView></ScrollView>
-				<View style={styles.footer}>
-					<TextInput
-						value={input}
-						onChangeText={(text) => setInput(text)}
-						placeholder="Signal Message"
-						style={styles.textInput}
-					/>
-					<TouchableOpacity activeOpacity={0.5} onPress={sendMessage}>
-						<Ionicons name="send" size={24} color="#2B68E6" />
-					</TouchableOpacity>
-				</View>
+				<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+					<>
+						<ScrollView></ScrollView>
+						<View style={styles.footer}>
+							<TextInput
+								value={input}
+								onChangeText={(text) => setInput(text)}
+								placeholder="Signal Message"
+								style={styles.textInput}
+							/>
+							<TouchableOpacity activeOpacity={0.5} onPress={sendMessage}>
+								<Ionicons name="send" size={24} color="#2B68E6" />
+							</TouchableOpacity>
+						</View>
+					</>
+				</TouchableWithoutFeedback>
 			</KeyboardAvoidingView>
 		</SafeAreaView>
 	)
