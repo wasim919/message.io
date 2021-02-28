765e2d471ac01a8434884a1789826ea31aef2da6
Wasim <wasimwik919@gmail.com>
2021-03-01 01:02:16 +0530

    message sending and receiving done

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,16 +1,31 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { StyleSheet, TouchableOpacity } from "react-native"
 import { ListItem, Avatar } from "react-native-elements"
+import { db } from "../firebase"
+import firebase from "firebase"
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
+	const [messages, setMessages] = useState([])
+	useEffect(() => {
+		const unsubscribe = db
+			.collection("chats")
+			.doc(id)
+			.collection("messages")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) => setMessages(snapshot.docs.map((doc) => doc.data())))
+		return unsubscribe
+	}, [])
 	return (
 		<TouchableOpacity onPress={() => enterChat(id, chatName)} activeOpacity={0.5}>
 			<ListItem key={id} bottomDivider>
-				<Avatar rounded source={{ uri: "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png" }} />
+				<Avatar
+					rounded
+					source={{ uri: messages?.[0]?.photoURL || "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png" }}
+				/>
 				<ListItem.Content>
 					<ListItem.Title style={{ fontWeight: "800" }}>{chatName}</ListItem.Title>
 					<ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-						This is a chat room.This is a chat room.This is a chat room.This is a chat room.
+						{messages?.[0]?.displayName}: {messages?.[0]?.message}
 					</ListItem.Subtitle>
 				</ListItem.Content>
 			</ListItem>
