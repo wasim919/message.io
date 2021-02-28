70c55d1d9ad27f16c5924dab1aae30dce404836f
Wasim <wasimwik919@gmail.com>
2021-02-28 21:27:23 +0530

    create chat room done

--- screens/AddChat.js
+++ screens/AddChat.js
@@ -1,14 +1,57 @@
-import React from "react"
+import React, { useLayoutEffect, useState } from "react"
 import { StyleSheet, Text, View } from "react-native"
+import { Icon, Input, Button } from "react-native-elements"
 
-const AddChat = () => {
+import { db } from "../firebase"
+
+const AddChat = ({ navigation }) => {
+	const [input, setInput] = useState("")
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerTitle: "Add a new chat",
+			headerBackTitle: "Chat"
+		})
+	}, [navigation])
+	const createChat = async () => {
+		await db
+			.collection("chats")
+			.add({
+				chatName: input
+			})
+			.then(() => {
+				navigation.goBack()
+			})
+			.catch((err) => alert(err.message))
+	}
 	return (
-		<View>
-			<Text>Add Chat screen</Text>
+		<View style={styles.screen}>
+			<Input
+				style={styles.inputContainer}
+				type="text"
+				value={input}
+				onChangeText={(text) => setInput(text)}
+				placeholder="Enter a chat name"
+				leftIcon={<Icon name="wechat" type="antdesign" size={24} color="black" />}
+				onSubmitEditing={createChat}
+			/>
+			<Button containerStyle={styles.button} title="Add new Chat" onPress={createChat} />
 		</View>
 	)
 }
 
 export default AddChat
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1
+		// justifyContent: "center",
+		// alignItems: "center"
+	},
+	inputContainer: {
+		// width: 300
+	},
+	button: {
+		// width: 200,
+		marginTop: 10
+	}
+})
