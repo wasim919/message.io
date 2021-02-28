16cef24a1009cb69f99f04c685fd2bcea8801d5d
Wasim <wasimwik919@gmail.com>
2021-02-28 15:56:20 +0530

    login/register ui done

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,59 @@
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import { StyleSheet, View, KeyboardAvoidingView } from "react-native"
+import { Input, Button, Text } from "react-native-elements"
+import { StatusBar } from "expo-status-bar"
 
 const RegisterScreen = () => {
+	const [name, setName] = useState("")
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const [imageURL, setImageURL] = useState("")
+	const register = () => {}
 	return (
-		<View>
-			<Text>Register Screen</Text>
-		</View>
+		<KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={20} style={styles.container}>
+			<StatusBar style="light" />
+			<Text h3 style={{ marginBottom: 50 }}>
+				Create a Signal account
+			</Text>
+			<View style={styles.inputContainer}>
+				<Input placeholder="Full Name" value={name} onChangeText={(text) => setName(text)} autoFocus type="text" />
+				<Input placeholder="Email" value={email} onChangeText={(text) => setEmail(text)} type="email" />
+				<Input
+					placeholder="Password"
+					secureTextEntry
+					value={password}
+					onChangeText={(text) => setPassword(text)}
+					autoFocus
+					type="password"
+				/>
+				<Input
+					placeholder="Profile Picture URL (optional)"
+					value={imageURL}
+					onChangeText={(text) => setImageURL(text)}
+					type="image"
+					onSubmitEditing={register}
+				/>
+			</View>
+			<Button raised containerStyle={styles.button} title="Register" onPress={register} />
+		</KeyboardAvoidingView>
 	)
 }
 
 export default RegisterScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		padding: 10,
+		backgroundColor: "white"
+	},
+	inputContainer: {
+		width: 300
+	},
+	button: {
+		width: 300,
+		marginTop: 10
+	}
+})
