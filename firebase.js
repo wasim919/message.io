57595b778b51d79d271b7d9c1a1165d66126e3f4
Wasim <wasimwik919@gmail.com>
2021-03-01 13:26:38 +0530

    config file changed

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore"
 import "firebase/auth"
 
 export const firebaseConfig = {
-	apiKey: "AIzaSyB00hDC4IJ2MKuav_UsoH7xDD-iMaeiSko",
-	authDomain: "signal-clone-4e975.firebaseapp.com",
-	projectId: "signal-clone-4e975",
-	storageBucket: "signal-clone-4e975.appspot.com",
-	messagingSenderId: "233911090773",
-	appId: "1:233911090773:web:adac78102e5fcb69289edc"
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: ""
 }
 
 let app
